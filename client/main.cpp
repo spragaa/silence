818a246da7f9e8b3a0bbf2621025a68e423dd923
Ryuji Kimura <ryujikimura92130@outlook.com>
2024-09-12 20:49:47 +0300

    setup project structure

---
+++ client/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "client.hpp"
+
+int main() {
+    print_hello();
+
+    return 0;
+}
---
+++ client/src/client.cpp
@@ -0,0 +1,5 @@
+#include "client.hpp"
+
+void print_hello() {
+    std::cout << "client is up and running" << std::endl;
+}
