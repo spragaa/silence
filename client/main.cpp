e56721fada7c4274db5303b3b8c9f38079f46bfb
prumat4 <didukhmaksym4@gmail.com>
2024-09-18 16:33:55 +0300

    improve user exp, menu, user metadata caching

--- client/main.cpp
+++ client/main.cpp
@@ -1,24 +1,17 @@
 #include "client.hpp"
 
 int main(int argc, char *argv[]) {
-    // std::string arg_nickname;
+    std::string arg_nickname;
 
-    // for (int i = 1; i < argc; ++i) {
-    //     std::string arg = argv[i];
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
 
-    //     if (arg.rfind("--nickname=", 0) == 0) {
-    //         arg_nickname = arg.substr(11);
-    //     }
-    // }
-
-    // if (!arg_nickname.empty()) {
-    //     std::cout << "Nickname: " << arg_nickname << std::endl;
-    // } else {
-    //     std::cout << "No nickname provided." << std::endl;
-    //     arg_nickname = "noname";
-    // }
-
-    Client client("127.0.0.1", 55555);
+        if (arg.rfind("--nickname=", 0) == 0) {
+            arg_nickname = arg.substr(11);
+        }
+    }
+    
+    Client client("127.0.0.1", 55555, arg_nickname);
     client.run();
 
     return 0;
--- common/src/user.cpp
+++ common/src/user.cpp
@@ -4,8 +4,12 @@ int User::user_id_counter = 1;
 
 User::User() : id(0), nickname(""), password("") {}
 
+User::User(const std::string& nick) : User() {
+    nickname = nick;
+}
+
 User::User(const std::string& nick, const std::string& pass) : id(user_id_counter++), nickname(
-		nick), password(pass) {
+		nick), password(pass), registered_timestamp(std::chrono::system_clock::now()) {
 		DEBUG_MSG("New user registered,  nickname: " + nickname + ", " + "password: " + password);
 }
 
@@ -19,6 +23,10 @@ std::string User::get_nickname() const noexcept {
 
 std::string User::get_password() const noexcept {
 	return password;
+}
+
+Timestamp User::get_registered_timestamp() const noexcept {
+    return registered_timestamp;
 }
 
 bool User::check_password(const std::string& pass) noexcept {
@@ -35,4 +43,70 @@ void User::set_nickname(const std::string& nick) noexcept {
 
 void User::set_password(const std::string& pass) noexcept {
     password = pass;
+}
+
+void User::set_registered_timestamp(const Timestamp& timestamp) noexcept {
+    registered_timestamp = timestamp;
+}
+
+nlohmann::json User::to_json() const {
+    nlohmann::json j;
+    j["id"] = id;
+    j["nickname"] = nickname;
+    j["password"] = password;
+    j["registered_timestamp"] = registered_timestamp.time_since_epoch().count();
+    return j;
+}
+
+User User::from_json(const nlohmann::json& j) {
+    User user;
+    user.id = j["id"];
+    user.nickname = j["nickname"];
+    user.password = j["password"];
+    user.registered_timestamp = Timestamp(std::chrono::nanoseconds(j["registered_timestamp"].get<int64_t>()));
+    return user;
+}
+
+void User::save_user_data_to_json(const std::string& filename) const {
+    nlohmann::json j = to_json();
+
+    std::filesystem::path filePath(filename);
+    std::filesystem::path dir = filePath.parent_path();
+    
+    try {
+        if (!std::filesystem::exists(dir)) {
+            if (std::filesystem::create_directories(dir)) {
+                DEBUG_MSG("Created directory: " + dir.string());
+            } else {
+                DEBUG_MSG("Failed to create directory: " + dir.string());
+                return;
+            }
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        DEBUG_MSG("Filesystem error: " + std::string(e.what()));
+        return;
+    }
+
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        file << j.dump(4);  // 4 spaces indent for pretty printing
+        file.close();
+        DEBUG_MSG("User data saved to " + filename);
+    } else {
+        DEBUG_MSG("Unable to open file for writing: " + filename);
+    }
+}
+
+User User::load_user_data_from_json(const std::string& filename) {
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        nlohmann::json j;
+        file >> j;
+        file.close();
+        DEBUG_MSG("User data loaded from " + filename);
+        return from_json(j);
+    } else {
+        DEBUG_MSG("Unable to open file for reading: " + filename);
+        return User();
+    }
 }
