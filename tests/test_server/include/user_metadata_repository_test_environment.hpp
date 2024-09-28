df09918bb09db1ae8612a597d55a10c452962a23
Ryuji Kimura <108902150+spragaa@users.noreply.github.com>
2024-09-28 23:27:17 +0300

    finish crud tests

--- tests/test_server/include/user_metadata_repository_test_environment.hpp
+++ tests/test_server/include/user_metadata_repository_test_environment.hpp
@@ -9,7 +9,6 @@ class UserMetadataRepositoryTestEnvironment : public ::testing::Environment {
     void SetUp() override {
         DEBUG_MSG("Setting up UserMetadataRepositoryTestEnvironment...");
         DEBUG_MSG("Executing " + std::string(SOURCE_DIR) + "/tests/test_server/scripts/setup_test_user_metadata.sh");
-        // --user=spraga --user_password=spraga -- should read from params
         system((std::string(SOURCE_DIR) + "/tests/test_server/scripts/setup_test_user_metadata.sh --user=spraga --user_password=spraga").c_str());
     }
     
