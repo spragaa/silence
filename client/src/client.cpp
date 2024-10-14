bcbdf4c86d155eeb038359f4c5f707eca8bc02da
Ryuji Kimura <108902150+spragaa@users.noreply.github.com>
2024-10-14 23:25:56 +0300

    improve user experience, move registation and authorization into separate func

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -7,7 +7,16 @@ Client::Client(const std::string& server_address,
 	, server_address(server_address)
 	, server_port(server_port)
 	, user(nick)
+	, is_authorized(false)
 {
+    DEBUG_MSG("[Client::Client] Trying to read user data from json...");
+    std::string user_data_filename = get_user_data_filename();
+    user = User::load_user_data_from_json(user_data_filename);
+    if (user.get_id() == 0) {
+        user = User();
+        user.set_nickname(nick);
+        DEBUG_MSG("[Client::Client] Temporary user created with nickname: '" + user.get_nickname() + "'");
+    }
 }
 
 User Client::get_user() {
@@ -15,246 +24,201 @@ User Client::get_user() {
 }
 
 void inline Client::show_actions() {
-	// there is always no nickname
-	std::cout << user.get_nickname() << ", please select the number of the action you would like to perform now from the list below:" << std::endl;
-	std::cout << "0. Register new user" << std::endl;
-	std::cout << "1. Authorize user" << std::endl;
-	std::cout << "2. Get list of chats" << std::endl;
-	std::cout << "3. Send message" << std::endl;
-	std::cout << "4. Get offline messages" << std::endl;
-	std::cout << "5. KYS" << std::endl;
+    std::cout << user.get_nickname() << ", please select the number of the action you would like to perform now from the list below:" << std::endl;
+    std::cout << "2. Get list of chats" << std::endl;
+    std::cout << "3. Send message" << std::endl;
+    std::cout << "4. Get offline messages" << std::endl;
+    std::cout << "5. Exit" << std::endl;
 
-	std::cout << "Input: ";
+    std::cout << "Input: ";
+}
+
+bool Client::is_registered() const noexcept {
+    return user.get_id() != 0;
+}
+
+void Client::register_user() {
+    std::string password;
+    std::string nickname = user.get_nickname();
+    std::cout << nickname << ", create password: ";
+    std::getline(std::cin, password);
+    
+    nlohmann::json request;
+    request["type"] = "register";
+    request["nickname"] = nickname;
+    request["password"] = password;
+
+    boost::asio::write(socket, boost::asio::buffer(request.dump() + "\r\n\r\n"));
+    DEBUG_MSG("[Client::register_user] Sending request: " + request.dump());
+    nlohmann::json response = nlohmann::json::parse(receive_response());
+    DEBUG_MSG("[Client::register_user] Received response: " + response.dump());
+
+    if(response["status"] == "success") {
+        if (response.contains("user_id") && !response["user_id"].is_null()) {
+            user.set_id(response["user_id"].get<int>());
+            DEBUG_MSG("[Client::register_user] User id set: " + std::to_string(user.get_id()));
+        } else {
+            WARN_MSG("[Client::register_user] User ID not provided in the response");
+        }
+        if (response.contains("registered_timestamp") && !response["registered_timestamp"].is_null()) {
+            std::chrono::nanoseconds ns(response["registered_timestamp"].get<int64_t>());
+            Timestamp registered_timestamp = std::chrono::system_clock::time_point(ns);
+            user.set_registered_timestamp(registered_timestamp);
+        } else {
+            WARN_MSG("[Client::register_user] Registered timestamp not provided in the response");
+        }
+
+        user.set_nickname(nickname);
+        user.set_password(password);
+        user.save_user_data_to_json(get_user_data_filename());
+        INFO_MSG("[Client::register_user] Registration successful. You can now authorize.")
+    } else if (response["status"] == "error") {
+        ERROR_MSG("[Client::register_user] Failed to register!");
+    }
+}
+
+void Client::authorize_user() {
+    std::string password;
+    std::cout << user.get_nickname() << ", please enter password: ";
+    std::getline(std::cin, password);
+
+    nlohmann::json request;
+    request["type"] = "authorize";
+    request["password"] = password;
+    request["nickname"] = user.get_nickname();
+    request["user_id"] = user.get_id();
+    DEBUG_MSG("[Client::authorize_user()] Sending request:" + request.dump());
+
+    try {
+        boost::asio::write(socket, boost::asio::buffer(request.dump() + "\r\n\r\n"));
+        INFO_MSG("[Client::authorize_user()] Authorization request sent successfully");
+
+        nlohmann::json response = nlohmann::json::parse(receive_response());
+        DEBUG_MSG("[Client::authorize_user()] Received response: " + response.dump());
+        
+        if(response["status"] == "success") {
+            is_authorized = true;
+            // if (response.contains("user_data") && !response["user_data"].is_null()) {
+                // nlohmann::json user_data = response["user_data"];
+                // this is not yet finished on server side
+                // if (user_data.contains("last_online_timestamp") && !user_data["last_online_timestamp"].is_null()) {
+                //     std::chrono::nanoseconds ns(user_data["last_online_timestamp"].get<int64_t>());
+                //     Timestamp last_online = std::chrono::system_clock::time_point(ns);
+                //     user.set_last_online_timestamp(last_online);
+                // }
+                // if (user_data.contains("is_online") && !user_data["is_online"].is_null()) {
+                //     user.set_online(user_data["is_online"].get<bool>());
+                // }
+            // }
+
+            user.save_user_data_to_json(get_user_data_filename());
+            INFO_MSG("[Client::authorize_user()] User data updated and saved");
+        } else if (response["status"] == "error") {
+            ERROR_MSG("[Client::authorize_user()] Failed to authorize user!");
+        } else {
+            ERROR_MSG("[Client::authorize_user()] Unexpected response from server");
+        }
+    } catch (const std::exception& e) {
+        ERROR_MSG("[Client::authorize_user()] Exception during authorization" + std::string(e.what()));
+    }
+
+    INFO_MSG("[Client::authorize_user()] Authorization process completed");
+}
+
+void Client::handle_user_actions() {
+    while (true) {
+        show_actions();
+        int action_type = -1;
+
+        while (true) {
+            std::cout << "Choose an action (0-5): ";
+            std::string input;
+            std::getline(std::cin, input);
+
+            try {
+                action_type = std::stoi(input);
+            } catch (std::invalid_argument) {
+                action_type = -1;
+            }
+
+            if (action_type >= 0 && action_type < 6) {
+                break;
+            }
+
+            std::cout << "Invalid input. Please enter a number between 0 and 5." << std::endl;
+        }
+
+        switch (action_type) {
+            case 0:
+                std::cout << "You are already registered and authorized." << std::endl;
+                break;
+            case 1:
+                std::cout << "You are already authorized" << std::endl;
+                break;
+            case 2:
+                std::cout << "TODO: Implement get list of chats" << std::endl;
+                break;
+            case 3:
+                std::cout << "TODO: Implement send message" << std::endl;
+                break;
+            case 4:
+                std::cout << "TODO: Implement get offline messages" << std::endl;
+                break;
+            case 5:
+                std::cout << "Exiting..." << std::endl;
+                return;
+            default:
+                std::cout << "Invalid action. Please try again." << std::endl;
+        }
+    }
 }
 
 void Client::run() {
-	try {
-		boost::asio::ip::tcp::resolver resolver(io_service);
-		boost::asio::ip::tcp::resolver::query query(server_address,
-		                                            std::to_string(server_port));
-		boost::asio::ip::tcp::resolver::iterator endpoint_iterator =
-			resolver.resolve(query);
-		boost::asio::connect(socket, endpoint_iterator);
+    try {
+        boost::asio::ip::tcp::resolver resolver(io_service);
+        boost::asio::ip::tcp::resolver::query query(server_address, std::to_string(server_port));
+        boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+        boost::asio::connect(socket, endpoint_iterator);
 
-		std::string user_data_filename = get_user_data_filename();
+        while (!is_registered()) {
+            WARN_MSG("[Client::run()] No existing user data found. Please register.");
+            register_user();
+            if (!is_registered()) {
+                WARN_MSG("[Client::run()] Registration failed. Would you like to try again? (y/n):");
+                std::string response;
+                std::getline(std::cin, response);
+                if (response != "y" && response != "Y") {
+                    INFO_MSG("[Client::run()] Exiting...");
+                    return;
+                }
+            }
+        }
 
-		try {
-			user = User::load_user_data_from_json(user_data_filename);
-			std::cout << "User data loaded successfully." << std::endl;
-		} catch (const std::exception& e) {
-			std::cout << "No existing user data found. Please register." << std::endl;
-		}
+        INFO_MSG("[Client::run()] User registered successfully");
 
-		// TODO: remembder nickname on the client side
-		while (true) {
-			bool is_registered = (user.get_id() != 0);
-			int action_type = -1;
+        while (!is_authorized) {
+            INFO_MSG("[Client::run()] Please authorize to continue.");
 
-			if (!is_registered) {
-				std::cout << "You need to register first." << std::endl;
-				action_type = 0;
-			} else {
-				while (true) {
-					show_actions();
-					std::cout << "Choose an action (0-5): ";
-					std::string input;
-					std::getline(std::cin, input);
+            authorize_user();
+            if (!is_authorized) {
+                WARN_MSG("[Client::run()] Authorization failed. Would you like to try again? (y/n): ");
+                std::string response;
+                std::getline(std::cin, response);
+                if (response != "y" && response != "Y") {
+                    INFO_MSG("[Client::run()] Exiting...");
+                    return;
+                }
+            }
+        }
 
-					//somehow here empty line is printed and nothing happens
-					try{
-						action_type = std::stoi(input);
-					} catch (std::invalid_argument) {
-						action_type = -1;
-					}
+        INFO_MSG("[Client::run()] User authorized successfully");
+        INFO_MSG("[Client::run()] Now you can start messaging!");
+        handle_user_actions();
 
-					if (action_type >= 0 && action_type < 6) {
-						break;
-					}
-
-					std::cout << "Invalid input. Please enter a number between 0 and 5." << std::endl;
-					show_actions();
-				}
-
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			}
-
-			nlohmann::json request;
-			nlohmann::json response;
-
-			switch (action_type) {
-					case 0: {
-						std::string nickname, password;
-						std::cout << "Enter nickname: ";
-						std::getline(std::cin, nickname);
-
-						std::cout << "Enter password: ";
-						std::getline(std::cin, password);
-
-						request["type"] = "register";
-						request["nickname"] = nickname;
-						request["password"] = password;
-
-						boost::asio::write(socket, boost::asio::buffer(request.dump() + "\r\n\r\n"));
-						nlohmann::json response = nlohmann::json::parse(receive_response());
-
-						std::cout << "Server response status: ";
-						if(response["status"] == "success") {
-							std::cout << "Success: " << response["response"] << std::endl;
-							if (response.contains("user_id") && !response["user_id"].is_null()) {
-								user.set_id(response["user_id"].get<int>());
-							} else {
-								std::cout << "Warning: User ID not provided in the response" << std::endl;
-							}
-							if (response.contains("registered_timestamp") && !response["registered_timestamp"].is_null()) {
-								std::chrono::nanoseconds ns(response["registered_timestamp"].get<int64_t>());
-								Timestamp registered_timestamp = std::chrono::system_clock::time_point(ns);
-								user.set_registered_timestamp(registered_timestamp);
-							} else {
-								std::cout << "Warning: User ID not provided in the response" << std::endl;
-							}
-
-							user.set_nickname(nickname);
-							user.set_password(password);
-							user.save_user_data_to_json(get_user_data_filename());
-							std::cout << "Registration successful. You can now use other actions" << std::endl;
-						} else if (response["status"] == "error") {
-							std::cout << "Error: " << response["response"] << std::endl;
-							std::cout << "Please try registering again." << std::endl;
-						}
-
-						DEBUG_MSG("Server response: " + response.dump());
-						break;
-					}
-					case 1: {
-						if (user.get_id() == 0) {
-							std::cout << "Error: You need to register first." << std::endl;
-							break;
-						}
-
-						std::string password;
-						std::cout << user.get_nickname() << ", please enter password: ";
-						std::getline(std::cin, password);
-
-						nlohmann::json request;
-						request["type"] = "authorize";
-						request["password"] = password;
-						request["nickname"] = user.get_nickname();
-						request["user_id"] = user.get_id();
-
-						DEBUG_MSG("Sending authorization request: " + request.dump());
-
-						try {
-							boost::asio::write(socket, boost::asio::buffer(request.dump() + "\r\n\r\n"));
-							INFO_MSG("Authorization request sent successfully");
-
-							nlohmann::json response = nlohmann::json::parse(receive_response());
-
-							if(response["status"] == "success") {
-								std::cout << "Success: " << response["message"] << std::endl;
-
-								if (response.contains("user_data") && !response["user_data"].is_null()) {
-									nlohmann::json user_data = response["user_data"];
-									if (user_data.contains("last_online_timestamp") && !user_data["last_online_timestamp"].is_null()) {
-										std::chrono::nanoseconds ns(user_data["last_online_timestamp"].get<int64_t>());
-										Timestamp last_online = std::chrono::system_clock::time_point(ns);
-										user.set_last_online_timestamp(last_online);
-									}
-									if (user_data.contains("is_online") && !user_data["is_online"].is_null()) {
-										user.set_online(user_data["is_online"].get<bool>());
-									}
-								}
-
-								user.save_user_data_to_json(get_user_data_filename());
-
-								std::cout << "User data updated and saved" << std::endl;
-							} else if (response["status"] == "error") {
-								std::cout << "Error: " << response["message"] << std::endl;
-							} else {
-								std::cout << "Unexpected response from server" << std::endl;
-							}
-						} catch (const std::exception& e) {
-							std::cerr << "Exception during authorization: " << e.what() << std::endl;
-						}
-
-						INFO_MSG("Authorization process completed");
-						break;
-					}
-					case 2: {
-						std::cout << "TODO: " << std::endl;
-						std::cout << "Getting list of all chats... " << std::endl;
-						std::cout << std::endl;
-						break;
-					}
-					// A sends a message to B
-					// on server we store a message from A to B with all needed metadata
-					// client has its own copy of the message stored for some time,
-					// lets say that client can store only last N messages
-					// to open chat, it makes request to the server to get last M messages
-					// to do so, server find last M messages where receiver or/and sender is A and B in the messages table
-					//
-					// ? on the client side I would like to have chats list ? --> chat class, but I don't think that we need it
-					// on the server side --> check how fast does the find last M messages query works in LARGE dbs
-					case 3: {
-						// move somewhere
-						if (user.get_id() == 0) {
-							std::cout << "Error: You need to register and authorize first." << std::endl;
-							break;
-						}
-
-						std::string receiver_nickname, message_text;
-						std::cout << "Provide receiver nickname: ";
-						std::getline(std::cin, receiver_nickname);
-
-						std::cout << "Provide the message for " << receiver_nickname << ": ";
-						std::getline(std::cin, message_text);
-
-						request["type"] = "send_message";
-						request["sender_id"] = user.get_id();
-						request["sender_nickname"] = user.get_nickname();
-						request["receiver_nickname"] = receiver_nickname;
-						request["message_text"] = message_text;
-						// handle mediahere
-
-						boost::asio::write(socket, boost::asio::buffer(request.dump() + "\r\n\r\n"));
-						nlohmann::json response = nlohmann::json::parse(receive_response());
-
-						// if(response["status"] == "success") {
-						// 	std::cout << "Message sent successfully." << std::endl;
-						// 	Message received_message = Message::from_json(response["message"]);
-						// 	messages.push_back(received_message);
-						// 	DEBUG_MSG("Message added: " + received_message.to_json().dump());
-						// } else if (response["status"] == "error") {
-						// 	std::cout << "Error: " << response["message"] << std::endl;
-						// }
-
-						INFO_MSG("Server response: " + response.dump());
-
-						break;
-					}
-					case 4: {
-						std::cout << "TODO" << std::endl;
-						std::cout << "Getting offline messages... " << std::endl;
-						std::cout << std::endl;
-						break;
-					}
-					case 5: {
-						std::cout << "KYS KYS KYS KYS KYS KYS KYS ";
-						std::cout << std::endl;
-						break;
-					}
-					default: {
-						if (!is_registered) {
-							std::cout << "You must register before performing other actions." << std::endl;
-							break;
-						}
-					}
-			}
-		}
-
-		socket.close();
-	} catch (const std::exception& e) {
-		std::cerr << "Exception in Client::run() : " << e.what() << std::endl;
-	}
+        socket.close();
+    } catch (const std::exception& e) {
+        ERROR_MSG("[Client::run()] " + std::string(e.what()));
+    }
 }
 
 std::string Client::read_user_text() const noexcept{
@@ -290,5 +254,5 @@ std::string Client::receive_response() {
 }
 
 std::string Client::get_user_data_filename() const noexcept {
-	return std::string(SOURCE_DIR) + "/user_data/" + "user_ " + user.get_nickname() + "_" + server_address + "_" + std::to_string(server_port) + ".json";
+	return std::string(SOURCE_DIR) + "/user_data/" + "user_" + user.get_nickname() + "_" + server_address + "_" + std::to_string(server_port) + ".json";
 }
