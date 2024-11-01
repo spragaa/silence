66f6d00cb67e5967caaa593896820a319cebfb70
Ryuji Kimura <108902150+spragaa@users.noreply.github.com>
2024-11-02 00:52:22 +0200

    initial setup of file_server_test, run int this error: what():  Header already registered

--- file_server/main.cpp
+++ file_server/main.cpp
@@ -1,8 +1,15 @@
 #include "file_server.hpp"
 
 int main() {
-	file_server::FileServer server(55544, 16, std::string(SOURCE_DIR) + "/file_server/media_file_system/", 1024 * 1024 * 1024);
+	file_server::FileServer server(
+	    55544, 
+		16, 
+		std::string(SOURCE_DIR) + "/file_server/media_file_system/", 
+		1024 * 1024 * 1024
+	);
+	    // Pistache::Http::Endpoint::shutdown(); needed???
 	server.init();
 	server.start();
+	
 	return 0;
 }
