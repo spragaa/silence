05a6c1185bb9f5b240f54031c38efe81e0bee736
spragaa <didukhmaksym4@gmail.com>
2024-11-09 15:00:39 +0200

    .

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,3 @@
-# ../build/tests/test_server/test_server
-# ../build/tests/test_common/test_common
-../build/tests/test_file_server/test_file_server
+../build/tests/test_server/test_server
+../build/tests/test_common/test_common
+# ../build/tests/test_file_server/test_file_server
