8e1fdbd8004c1812e5eb5d76c5290348a935a41d
spragaa <didukhmaksym4@gmail.com>
2024-11-14 00:09:08 +0200

    add elgamal tests, 6 of them are somehow failing :)

--- common/crypto/src/crypto_utils.cpp
+++ common/crypto/src/crypto_utils.cpp
@@ -1,4 +1,4 @@
-#include "utils.hpp"
+#include "crypto_utils.hpp"
 
 #include <sstream>
 #include <iomanip>
@@ -7,17 +7,17 @@ namespace common {
 namespace crypto {
 
 cpp_int hex_to_cpp_int(const std::string& hex) {
-    cpp_int result;
-    std::stringstream ss;
-    ss << std::hex << hex;
-    ss >> result;
-    return result;
+	cpp_int result;
+	std::stringstream ss;
+	ss << std::hex << hex;
+	ss >> result;
+	return result;
 }
 
 std::string cpp_int_to_hex(const cpp_int& num) {
-    std::stringstream ss;
-    ss << std::hex << num;
-    return ss.str();
+	std::stringstream ss;
+	ss << std::hex << num;
+	return ss.str();
 }
 
 } // namespace common
