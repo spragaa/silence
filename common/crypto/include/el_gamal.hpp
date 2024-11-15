a2ffd8ca9888882747fa3aa01c52f20302db3151
spragaa <didukhmaksym4@gmail.com>
2024-11-15 19:49:22 +0200

    move some methods from ElGamal class to crypto_utils

--- common/crypto/include/el_gamal.hpp
+++ common/crypto/include/el_gamal.hpp
@@ -3,7 +3,6 @@
 #include "crypto_utils.hpp"
 
 #include <boost/multiprecision/cpp_int.hpp>
-#include <random>
 #include <array>
 #include <vector>
 
@@ -20,16 +19,6 @@ public:
 	const KeyPair& get_keys() const;
 	EncryptedMessage encrypt(const cpp_int& message, const cpp_int& recipient_public_key);
 	cpp_int decrypt(const EncryptedMessage& encrypted_message);
-
-protected:
-    bool is_prime(const cpp_int& n, int iterations = 50);
-    bool validate_parameters(const cpp_int& p, const cpp_int& g);
-    cpp_int generate_safe_prime(size_t bits);
-    cpp_int find_generator(const cpp_int& p);	
-    bool miller_rabin_test(const cpp_int& n, const cpp_int& a);
-
-	cpp_int generate_random(const cpp_int& min, const cpp_int& max);
-	cpp_int modular_pow(const cpp_int& base, const cpp_int& exponent, const cpp_int& modulus);
 
 private:
 	cpp_int p;
